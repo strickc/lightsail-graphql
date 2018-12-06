c7dce2efc8351225c23a7c0a565c6227e42d4cce
connor <connor@debian-connor.strick.net>
2018-12-06 15:36:21 -0700

    Install docker-compose

--- scripts/install-docker.sh
+++ scripts/install-docker.sh
@@ -22,3 +22,7 @@ sudo add-apt-repository \
 
 sudo apt-get update
 sudo apt-get install docker-ce
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.23.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version
