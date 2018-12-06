917270ee0a8d73efe3060707d80ec63ed4fc1d8c
connor <connor@debian-connor.strick.net>
2018-12-06 15:22:13 -0700

    script to install docker

---
+++ scripts/install-docker.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# stop script on error
+set -e
+
+sudo apt-get update
+
+# Install packages to allow apt to use a repository over HTTPS:
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+
+# Add Docker’s official GPG key:
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+sudo apt-get update
+sudo apt-get install docker-ce
